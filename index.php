ec4e74a2852e465a832e41f1a6bc993c40013624
Pierre-Antoine Navarro <pan-navarro@outlook.fr>
2021-03-29 19:17:16 +0200

    ajout du fichier de test de materialize et du footer

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" type="text/css" href="style/style.css">
+    <!-- <link rel="stylesheet" type="text/css" href="style/style.css">  -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <meta charset="utf-8">
 
     <title>Accueil</title>
@@ -102,9 +103,30 @@
 
 
 
-    <footer>
-      <div id="g"> <p> Pour nous contacter veuillez vous adresser ......© 2020</p> </div>
-
-    </footer>
+	<footer class="page-footer">
+	  <div class="container">
+		<div class="row">
+		  <div class="col l6 s12">
+			<h5 class="white-text">Footer Content</h5>
+			<p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+		  </div>
+		  <div class="col l4 offset-l2 s12">
+			<h5 class="white-text">Links</h5>
+			<ul>
+			  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+			  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+			  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+			  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+			</ul>
+		  </div>
+		</div>
+	  </div>
+	  <div class="footer-copyright">
+		<div class="container">
+		© 2014 Copyright Text
+		<a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+		</div>
+	  </div>
+	</footer>
 </body>
 </html>
---
+++ test.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+ <head>
+   <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+   <link rel="shortcut icon" type="image/ico" href="img/favicon.gif" />
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+   <title> titre </title>
+ </head>
+ <body>
+    <div class="container">
+
+	<nav>
+     <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Logo</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">JavaScript</a></li>
+      </ul>
+     </div>
+	</nav
+
+    </div>
+ </body>
+</html>
