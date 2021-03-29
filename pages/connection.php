ea72a6e274d8c08a942c0a4268bb0da535ab6c20
KheopsEver <labrossh@etu.univ-savoie.fr>
2021-03-29 20:51:43 +0200

    finissiont page connection

--- pages/connection.php
+++ pages/connection.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" type="text/css" href="../style/connexion.css">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <meta charset="utf-8">
 
     <title>Connexion</title>
@@ -13,14 +13,18 @@
 
 <body>
 
-
-    <section>
-        <div id="d">
-            <h1> <a href="../index.php">Retour à l'accueil</a>  </h1>
-
-        </div>
-    </section>
-
+<div class="container">
+    <div class="navbar">
+      <nav>
+        <div class="nav-wrapper grey darken-4">
+          <a href="#!" class="brand-logo">Logo</a>
+          <ul class="right hide-on-med-and-down">
+          <li><a href="pages/connection.php">Se connecter </a></li>
+          <li><a href="traitement/traitementDeconnection.php">Se déconnecter</a></li>
+          </ul>
+      </div>
+      </nav>
+    </div>
 
     <section>
 
@@ -29,20 +33,25 @@
         <div id="e">  <h2>Entrez vos identifiants</h2>
            <br>
            <br>
+           <div class="row">
             <form id="Connexion" method="post" action="../traitement/traitementConnection.php" >
+              <div class="col s6">
               <p>
                 <label for="email">Adresse mail :</label> <br />
                 <input type="email" name="email" id="email" size="60" />
               </p>
-
+              </div>
+              <div class="col s6">
               <p>
                 <label for="password">Mot de passe :</label> <br />     
                 <input type="password" name="password" id="password" size="60" />
               </p>
+            </div>
 
-              <input type="submit" name="connecter" id="connecter" value="Se connecter"/>
+              <input class="waves-effect blue-grey darken-2 btn" type="submit" name="connecter" id="connecter" value="Se connecter"/>
 
             </form>
+          </div>
         </div>
 
 
@@ -61,9 +70,23 @@
 
 
 
-    <footer>
-      <div id="g"> <p> Pour nous contacter veuillez vous adresser ......© 2020</p> </div>
-
+    <footer class="page-footer grey darken-4">
+      <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+        <h5 class="white-text">Contact</h5>
+        <p class="grey-text text-lighten-4">Si vous voulez nous contacter veuillez vous addresser au contact suivant</p>
+        </div>
+        <div class="col l4 offset-l2 s12">
+        <h5 class="white-text">Coordonnée</h5>
+        <ul>
+          <li><a class="grey-text text-lighten-3" href="#!">06 06 06 06 06 06</a></li>
+          <li><a class="grey-text text-lighten-3" href="#!">3 Rue de la Boustifaille cedex</a></li>
+          <li><a class="grey-text text-lighten-3" href="#!">Badger@gmail.com</a></li>
+          <li><a class="grey-text text-lighten-3" href="#!">Paris 74000</a></li>
+        </ul>
+        </div>
+      </div>
     </footer>
 </body>
 </html>
